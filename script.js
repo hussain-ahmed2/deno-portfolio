e36f3ff83cb2273925843598f4d8f30ee84838c4
Hussain Ahmed <hussainahmed.vu@gmail.com>
2024-11-15 18:41:23 +0600

    single page portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+});
